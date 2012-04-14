095f87834dd10e8f5905442712affab4d890e1fc
Liu Cheng <ccpp132@gmail.com>
2012-04-14 11:55:52 +0800

    adjust c-offset

--- personal/c-mode-setting.el
+++ personal/c-mode-setting.el
@@ -1,8 +1,8 @@
 ; c-mode
-(setq c-basic-offset 4)
 (setq-default indent-tabs-mode nil)
 (add-hook 'c-mode-common-hook
 	  (lambda ()
 	    (linum-mode)
 	    (local-set-key "\r" 'newline-and-indent)
-            (c-set-style "k&r")))
+            (c-set-style "k&r")
+            (setq c-basic-offset 4)))
